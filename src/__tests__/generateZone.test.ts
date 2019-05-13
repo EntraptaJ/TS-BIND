4e472470b6cd8db410e6c4baa8d02fcd981444f2
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-13 13:57:27 -0500

    test: Add more testing for generatezone

--- src/__tests__/generateZone.test.ts
+++ src/__tests__/generateZone.test.ts
@@ -1,6 +1,7 @@
-import { generateZoneFile } from '../createZone';
+import { generateZoneFile, parseZoneFile } from '../'
+import { ZONE } from '../types'
 import fs from 'fs-extra';
-const zone = {
+const zone: ZONE = {
 	$origin: 'net.kristianjones.xyz.',
 	soa: {
 		contact: 'me.kristianjones.xyz',
@@ -10,12 +11,28 @@ const zone = {
 		expire: '604800',
 		mttl: '1800'	
 	},
-	ns: [{ host: '@', value: 'dnstest.kristianjones.xyz.' }],
+	ns: [{ host: '@', ttl: 300,value: 'dnstest.kristianjones.xyz.' }],
 	a: [{ host: 'www', value: '192.168.1.118'}, { host: 'hello', value: '1.0.0.1'}],
-	aaaa: [{ host: '@', value: '2001:4860:4860::8888' }]
+	aaaa: [{ host: '@', value: '2001:4860:4860::8888' }],
+	txt: [{host: 'hllo', value: 'HOIDNKOHHF'}]
 
 }
+
+describe('Generate Zone Files', () => {
+	test('Generate Zone Files', async () => await expect(generateZoneFile(zone)).resolves.not.toThrow());
+
+	test('Generate Tested Zone File', async () => {
+		const expectZone: ZONE = {...zone, ptr: [], cname: []}
+		const zoneOBJ = await generateZoneFile(zone)
+		const loadedZone = await parseZoneFile(zoneOBJ);
+		await expect(loadedZone).toStrictEqual(expectZone);
+	})
+	
+
+
+})
 
 test('Generate Zone Files', async () => {
 	await expect(generateZoneFile(zone)).resolves.not.toThrow()
 });
+
