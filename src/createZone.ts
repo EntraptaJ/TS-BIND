faa8d71e2e3bf6f36272b863fc352e71cde78d0e
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-13 13:57:12 -0500

    feat: refactor multiple 'es' consts to a single map.

--- src/createZone.ts
+++ src/createZone.ts
@@ -1,33 +1,40 @@
 import { ZONE } from './types';
 
 export const generateZoneFile = async (zone: ZONE) => {
-  const Aes = zone.a ? zone.a.map(a => `\n${a.host}   A     ${a.value}`).join('') : [];
-  const AAAAs = zone.aaaa ? zone.aaaa.map(aaaa => `\n${aaaa.host} AAAA ${aaaa.value}`).join('') : [];
-  const NSes = zone.ns
-    ? zone.ns.map(ns => `\n${ns.host}     NS    ${/\.\D+\.$/.test(ns.value) ? ns.value : `${ns.value}.`}`).join('')
-    : [];
-  const CNAMEs = zone.cname ? zone.cname.map(cname => `\n${cname.host} CNAME ${cname.value}`).join('') : [];
+  const Records = await Promise.all(
+    Object.keys(zone).map(async a => {
+      let RCD = [];
+      switch (typeof zone[a]) {
+        case 'object':
+          await Promise.all(
+            Object.keys(zone[a]).map(async a2 => {
+              switch (typeof zone[a][a2]) {
+                case 'object':
+                  if (zone[a][a2].host)
+                    RCD.push(
+                      `\n${zone[a][a2].host}     ${
+                        typeof zone[a][a2].ttl !== 'undefined' ? zone[a][a2].ttl : ''
+                      }    IN    ${a.toUpperCase()}     ${zone[a][a2].value}`,
+                    );
+                  break;
+              }
+            }),
+          );
+          break;
+      }
+      return RCD.join('');
+    }),
+  );
   const zoneText = `
 $ORIGIN ${/\.\D+\.$/.test(zone.$origin) ? zone.$origin : `${zone.$origin}.`}\n
 @ 3600 SOA ${/\.\D+\.$/.test(zone.ns[0].value) ? zone.ns[0].value : `${zone.ns[0].value}.`} (
-        ${zone.soa.contact}      ; address of responsible party
-        ${zone.soa.serial}                   ; serial number
-        ${zone.soa.refresh}                       ; refresh period
-        ${zone.soa.retry}                        ; retry period
-        ${zone.soa.expire}                     ; expire time
-        ${zone.soa.mttl}                     ) ; minimum ttl
+  ${zone.soa.contact}      ; address of responsible party
+  ${zone.soa.serial}                   ; serial number
+  ${zone.soa.refresh}                       ; refresh period
+  ${zone.soa.retry}                        ; retry period
+  ${zone.soa.expire}                     ; expire time
+  ${zone.soa.mttl}                     ) ; minimum ttl
 
-; NS Records
-${NSes}
-
-; A Records
-${Aes}
-
-; AAAA Records
-${AAAAs}
-
-; CNAME Records
-${CNAMEs}
-`;
+${Records.join('')}`;
   return zoneText;
 };
