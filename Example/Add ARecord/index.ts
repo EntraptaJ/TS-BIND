51a6b48610f915e66690d7d0286a6cd52c8588fb
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-15 11:33:56 -0500

    docs: Cleanup docs

--- Example/Add ARecord/index.ts
+++ Example/Add ARecord/index.ts
@@ -2,11 +2,11 @@ import { parseZoneFile, generateZoneFile } from 'ts-zone-file'
 import { readFile, writeFile } from 'fs-extra';
 
 const Example1 = async () => {
-  const file = await readFile('./example.com')
+  const file = await readFile('./example.txt')
   const zone = await parseZoneFile(file.toString())
   zone.a.push({ host: 'www', value: '1.1.1.1' })
   const zoneString = await generateZoneFile(zone)
-  await writeFile('./example.com', zoneString)
+  await writeFile('./example.txt', zoneString)
 }
 
 Example1()
