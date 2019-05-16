4cd8fa988d37d453989b429baa92cb853d846f38
Kristian Jones <kristianfjonesis@gmail.com>
2019-05-16 11:08:49 -0500

    fix: Fix saving NS records

--- src/__tests__/generateZone.test.ts
+++ src/__tests__/generateZone.test.ts
@@ -11,7 +11,7 @@ const SAMPLE1: ZONE = {
 		expire: '604800',
 		mttl: '1800'	
 	},
-	ns: [{ host: '@', ttl: 300,value: 'dnstest.kristianjones.xyz' }],
+	ns: [{ host: '@', ttl: 300,value: 'dnstest' }],
 	a: [{ host: 'www', value: '192.168.1.118'}, { host: 'hello', value: '1.0.0.1'}],
 	aaaa: [{ host: '@', value: '2001:4860:4860::8888' }],
 	txt: [{host: 'hllo', value: 'HOIDNKOHHF'}],
@@ -71,7 +71,7 @@ describe('Generate Zone Files', () => {
 
 	describe('Test Generated Files', () => {
 		test('Generate Tested Zone File', async () => {
-			const expectZone: ZONE = {...SAMPLE1, ns: [{ host: '@', ttl: 300, value: 'dnstest.kristianjones.xyz.'}], ptr: [], cname: [], mx: []}
+			const expectZone: ZONE = {...SAMPLE1, ns: [{ host: '@', ttl: 300, value: 'dnstest'}], ptr: [], cname: [], mx: []}
 			const zoneOBJ = await generateZoneFile(SAMPLE1)
 			const loadedZone = await parseZoneFile(zoneOBJ);
 			await expect(loadedZone).toStrictEqual(expectZone);
--- src/createZone.ts
+++ src/createZone.ts
@@ -9,7 +9,11 @@ export const generateZoneFile = async (zone: ZONE): Promise<string> => {
   const valuemap = map.filter(a => a[0] !== 'srv' && a[0] !== 'mx') as [string, string | number | SOA | VRECORD[]][];
   const prefmap = map.filter(a => a[0] === 'mx').sort() as [string, MXRECORD[]][];
   const srvmap = map.filter(a => a[0] === 'srv').sort() as [string, SRVRECORD[]][];
-  const promises = [Promise.all(valuemap.map(async item => await processValueOBJ(item))), Promise.all(srvmap.map(async item => await processSRVOBJ(item))), Promise.all(prefmap.map(async item => await processPREFOBJ(item)))];
+  const promises = [
+    Promise.all(valuemap.map(async item => await processValueOBJ(item))),
+    Promise.all(srvmap.map(async item => await processSRVOBJ(item))),
+    Promise.all(prefmap.map(async item => await processPREFOBJ(item))),
+  ];
   await Promise.all(promises);
   const zoneText = `
 $ORIGIN ${/\.\D+\.$/.test(zone.$origin) ? zone.$origin : `${zone.$origin}.`}\n
@@ -28,27 +32,27 @@ ${Object.entries(RCD)
 
 const processValueOBJ = async ([key, a]: [string, string | number | SOA | VRECORD[]]) =>
   Array.isArray(a)
-    ? a.sort((a,b) => a.host < b.host ? -1 : 1).map(async obj => {
-        const line = `\n${obj.host}  ${typeof obj.ttl !== 'undefined' ? obj.ttl : ''}  IN ${key.toUpperCase()}   ${await formatValue(obj.value, key)}`;
-        if (!RCD[key]) RCD[key] = [line];
-        else RCD[key].push(line);
-      })
+    ? a
+        .sort((a, b) => (a.host < b.host ? -1 : 1))
+        .map(async obj => {
+          const line = `\n${obj.host}\t${obj.ttl ? `${obj.ttl}\t` : ''}IN\t${key.toUpperCase()}\t${obj.value}`;
+          if (!RCD[key]) RCD[key] = [line];
+          else RCD[key].push(line);
+        })
     : '';
 
 const processSRVOBJ = async ([key, a]: [string, SRVRECORD[]]) =>
   a.map(async obj => {
-    const line = `\n${obj.service}.${obj.protocol}.${obj.host}  ${typeof obj.ttl !== 'undefined' ? obj.ttl : ''}   IN ${key.toUpperCase()} ${obj.priority} ${
-      obj.weight
-    } ${obj.port}  ${await formatValue(obj.target, key)}`;
+    const line = `\n${obj.service}.${obj.protocol}.${obj.host}\t${obj.ttl ? `${obj.ttl}\t` : ''}IN\t${key.toUpperCase()}\t${obj.priority}\t${obj.weight}\t${
+      obj.port
+    }\t${obj.target}`;
     if (!RCD[key]) RCD[key] = [line];
     else RCD[key].push(line);
   });
 
 export const processPREFOBJ = async ([key, a]: [string, MXRECORD[]]) =>
   a.map(async obj => {
-    const line = `\n${obj.host}  ${typeof obj.ttl !== 'undefined' ? obj.ttl : ''} IN ${key.toUpperCase()}   ${obj.preference}  ${await formatValue(obj.value, key)}`;
+    const line = `\n${obj.host}\t${obj.ttl ? `${obj.ttl}\t` : ''}IN\t${key.toUpperCase()}\t${obj.preference}\t${obj.value}`;
     if (!RCD[key]) RCD[key] = [line];
     else RCD[key].push(line);
   });
-
-const formatValue = async (value: string, key: string) => (key.toUpperCase() == 'NS' ? (/\.\D+\.$/.test(value) ? value : `${value}.`) : value);
