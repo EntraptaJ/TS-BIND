41ecf9cd2bf78002ef770ac3ba2f10f1a48ec06b
Kristian F Jones <me@kristianjones.xyz>
2019-05-29 19:21:03 -0500

    fix: Stuff

--- src/createConfig.ts
+++ src/createConfig.ts
@@ -11,33 +11,46 @@ options {
     ${config.options.directory ? `directory "${config.options.directory}";` : ``}
     ${config.options.pidFile ? `pid-file "${config.options.pidFile}";` : ``}
 
-    ${config.options.listenOn ? `listen-on {\n${config.options.listenOn.map((on) => `\t\t${on};\n`).join('')}\t  };` : ``}
+    ${config.options.listenOn ? `listen-on {\n${config.options.listenOn.map(on => `\t\t${on};\n`).join('')}\t  };` : ``}
 
-    ${config.options.allowTransfer ? `allow-transfer {\n${config.options.allowTransfer.map((transfer) => `\t\t${transfer};\n`).join('')}\t  };` : ``}
+    ${config.options.allowTransfer ? `allow-transfer {\n${config.options.allowTransfer.map(transfer => `\t\t${transfer};\n`).join('')}\t  };` : ``}
 
-    ${config.options.allowRecursion ? `allow-recursion {\n${config.options.allowRecursion.map((recursion) => `\t\t${recursion};\n`).join('')}\t  };` : ``}
+    ${config.options.allowRecursion ? `allow-recursion {\n${config.options.allowRecursion.map(recursion => `\t\t${recursion};\n`).join('')}\t  };` : ``}
 
     ${typeof config.options.recursion !== 'undefined' ? `recursion ${config.options.recursion ? `yes` : `no`};` : ''}
 
     ${config.options.dnssec ? `` : ``}
-}
+};
 
-${config.keys ? config.keys.map((key) => `key "${key.name}" {
+${
+  config.keys
+    ? config.keys
+        .map(
+          key => `key "${key.name}" {
 \talgorithm ${key.algorithm};
 \tsecret "${key.secret}";
-};\n\n`).join('') : ''}
+};\n\n`,
+        )
+        .join('')
+    : ''
+}
 
-${config.zones.map(zone => `zone "${zone.name}" {
+${config.zones
+  .map(
+    zone => `zone "${zone.name}" {
 \ttype ${zone.type};
 \tfile "${zone.file}";
-${zone.keyDirectory ? `\tkey-directory "${zone.keyDirectory}";` : ''}
-${zone.autoDNSSEC ? `\tauto-dnssec ${zone.autoDNSSEC};` : ''}
-${typeof zone.notify !== 'undefined' ? `\tnotify ${zone.notify ? `yes` : `no`};` : `` }
-${typeof zone.inlineSigning !== 'undefined' ? `\tinline-signing ${zone.inlineSigning ? `yes` : `no`};` : `` }
-${zone.updatePolicy ? `\tupdate-policy { grant ${zone.updatePolicy.grant} zonesub ${zone.updatePolicy.zonesub}; };` : `` }
-${zone.allowTransfer ? `\tallow-transfer {\n${zone.allowTransfer.map((transfer) => `\t\t${transfer};\n`).join('')}\t  };` : ``}
-${zone.alsoNotify ? `\talso-notify {\n${zone.alsoNotify.map((notify) => `\t\t${notify};\n`).join('')}\t  };` : ``}
-};\n\n`).join('')}
+${zone.keyDirectory ? `\tkey-directory "${zone.keyDirectory}";\n` : ''}${zone.autoDNSSEC ? `\tauto-dnssec ${zone.autoDNSSEC};\n` : ''}${
+      typeof zone.notify !== 'undefined' ? `\tnotify ${zone.notify ? `yes` : `no`};\n` : ``
+    }
+${typeof zone.inlineSigning !== 'undefined' ? `\tinline-signing ${zone.inlineSigning ? `yes` : `no`};\n` : ``}${
+      zone.updatePolicy ? `\tupdate-policy { grant ${zone.updatePolicy.grant} zonesub ${zone.updatePolicy.zonesub}; };\n` : ``
+    }${zone.allowTransfer ? `\tallow-transfer {\n${zone.allowTransfer.map(transfer => `\t\t${transfer};\n`).join('')}\t  };\n` : ``}${
+      zone.alsoNotify ? `\talso-notify {\n${zone.alsoNotify.map(notify => `\t\t${notify};`).join('')}\t\n };\n` : ``
+    }
+};\n\n`,
+  )
+  .join('')}
     `;
 
   return configString;
